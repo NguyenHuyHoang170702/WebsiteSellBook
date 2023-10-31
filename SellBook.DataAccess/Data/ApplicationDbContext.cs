ed18670d9b5fef0206b92bec18a1274fb752b7e1
011223345aA <huyhoang11202@gmail.com>
2023-10-31 15:46:08 +0700

    Add sample data for Product

--- SellBook.DataAccess/Data/ApplicationDbContext.cs
+++ SellBook.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,51 @@ namespace SellBook.DataAccess
 
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			#region Add sample data for Product
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Product_Id = 1,
+					Title = "TEST1",
+					ProductDescription = "1TTTTT",
+					Author = "LOL",
+					ISBN = "QWE",
+					ListPrice = 10,
+					Price = 10,
+					Price50 = 8,
+					Price100 = 6
+				},
+				new Product
+				{
+					Product_Id = 2,
+					Title = "TEST2",
+					ProductDescription = "2TTTTT",
+					Author = "LOL",
+					ISBN = "QWE",
+					ListPrice = 10,
+					Price = 10,
+					Price50 = 8,
+					Price100 = 6
+				},
+				new Product
+				{
+					Product_Id = 3,
+					Title = "TEST3",
+					ProductDescription = "3TTTTT",
+					Author = "LOL",
+					ISBN = "QWE",
+					ListPrice = 10,
+					Price = 10,
+					Price50 = 8,
+					Price100 = 6
+				}
+			);
+			#endregion
+
+		}
+
 
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Product> Products { get; set; }

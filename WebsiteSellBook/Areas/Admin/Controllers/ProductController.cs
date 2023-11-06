bfbe402f3ee4f3d4418d8ff58a1629b40a7ca58a
011223345aA <huyhoang11202@gmail.com>
2023-11-06 16:00:40 +0700

    CRUD Product

--- WebsiteSellBook/Areas/Admin/Controllers/ProductController.cs
+++ WebsiteSellBook/Areas/Admin/Controllers/ProductController.cs
@@ -21,5 +21,68 @@ namespace WebsiteSellBook.Areas.Admin.Controllers
 			}
 			return View();
 		}
+
+		[HttpGet]
+		public IActionResult CreateProduct()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult CreateProduct(Product product)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Product.Add(product);
+				_unitOfWork.Save();
+				TempData["Success"] = "Create new product successful !!!";
+				TempData["Title"] = "Create product";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+
+		public IActionResult RemoveProduct(int id)
+		{
+			var exitId = _unitOfWork.Product.Get(item => item.Product_Id == id);
+			if (exitId != null)
+			{
+				_unitOfWork.Product.Remove(exitId);
+				_unitOfWork.Save();
+				TempData["Success"] = "Remove product successful !!!";
+				TempData["Title"] = "Remove product";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult EditProduct(int id)
+		{
+			var exitId = _unitOfWork.Product.Get(item => item.Product_Id == id);
+			if (exitId != null)
+			{
+				return View(exitId);
+			}
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult EditProduct(Product product)
+		{
+
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Product.Update(product);
+				_unitOfWork.Save();
+				TempData["Success"] = "Edit product successful !!!";
+				TempData["Title"] = "Edit product";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
 	}
 }

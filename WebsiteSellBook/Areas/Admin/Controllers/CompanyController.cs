d63357dd8e6a8d071485d07b9416b108e5603139
011223345aA <huyhoang11202@gmail.com>
2023-12-09 01:22:00 +0700

    fix update company

--- WebsiteSellBook/Areas/Admin/Controllers/CompanyController.cs
+++ WebsiteSellBook/Areas/Admin/Controllers/CompanyController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SellBook.DataAccess.Repository.IRepository;
+using SellBook.Models;
+using SellBook.Models.ViewModels;
+using SQLitePCL;
 
 namespace WebsiteSellBook.Areas.Admin.Controllers
 {
@@ -14,6 +17,56 @@ namespace WebsiteSellBook.Areas.Admin.Controllers
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpGet]
+		public IActionResult CreateAndUpdateCompany(int? id)
+		{
+			try
+			{
+				if (id == null || id == 0)
+				{
+					ViewBag.Title = "Create Company";
+					return View();
+				}
+				else
+				{
+					var exitCompany = _unitOfWork.Company.Get(item => item.CpmpanyId == id);
+					ViewBag.Title = "Edit Company";
+					return View(exitCompany);
+				}
+			}
+			catch (Exception ex)
+			{
+				return View(ex.Message);
+			}
+		}
+
+		[HttpPost]
+		public IActionResult CreateAndUpdateCompany(Company? company)
+		{
+			if (ModelState.IsValid)
+			{
+				if (company.CpmpanyId == 0)
+				{
+					_unitOfWork.Company.Add(company);
+					_unitOfWork.Save();
+					TempData["Success"] = "Create new Company successful !!!";
+					TempData["Title"] = "Create Company";
+				}
+				else
+				{
+					_unitOfWork.Company.Update(company);
+					_unitOfWork.Save();
+					TempData["Success"] = "Update Company successful !!!";
+					TempData["Title"] = "Update Company";
+				}
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
 		}
 
 

4a2129bd7722ac6fa688d48194f98ec3f8e6063a
011223345aA <huyhoang11202@gmail.com>
2023-12-08 15:50:17 +0700

    api and ajax get all company

--- WebsiteSellBook/Areas/Admin/Controllers/CompanyController.cs
+++ WebsiteSellBook/Areas/Admin/Controllers/CompanyController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SellBook.DataAccess.Repository.IRepository;
 
 namespace WebsiteSellBook.Areas.Admin.Controllers
 {
+	[Area("Admin")]
 	public class CompanyController : Controller
 	{
+		private readonly IUnitOfWork _unitOfWork;
+		public CompanyController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+
+
+		#region API Calls
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var getData = _unitOfWork.Company.GetAll().ToList();
+			return Json(new { data = getData });
+		}
+		#endregion
 	}
 }
---
+++ WebsiteSellBook/wwwroot/js/company.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+$(document).ready(() => {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $("#tblCompanyList").DataTable({
+        "ajax": {
+            url: '/admin/company/getall'
+        },
+        "columns": [
+            { data: 'companyName', "Width": "10%" },
+            { data: 'stressAddress', "Width": "20%" },
+            { data: 'city', "Width": "10%" },
+            { data: 'state', "Width": "10%" },
+            { data: 'postalCode', "Width": "10%" },
+            { data: 'phoneNumber', "Width": "15%" },
+            {
+                data: 'cpmpanyId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/company/createandupdatecompany?id=${data}" class="btn btn-success"><i class="bi bi-pencil-square" style="height:30px; cursor:pointer"></i>  Edit</a>
+						&nbsp;
+						<a  onClick=Delete('/admin/company/deletecompany/${data}') class="btn btn-primary"><i class="bi bi-trash" style="height:30px; cursor:pointer"></i> Delete</a>
+                    </div>`
+                }, "Width": "25%"
+            }
+        ],
+
+    });
+}

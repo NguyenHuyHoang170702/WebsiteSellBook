801a9bdd9b1b43c46f3c3998f3b0a9784f7553f8
011223345aA <huyhoang11202@gmail.com>
2023-12-19 15:36:41 +0700

    add order header and order details

---
+++ SellBook.DataAccess/Migrations/20231219083445_AddOrderHeaderAndOrderDetails.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SellBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderHeaderAndOrderDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShippingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderTotal = table.Column<double>(type: "float", nullable: false),
+                    OrderStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrackingNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Carrier = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaymentDueDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StressAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderHeaders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderHeaderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderHeaders_OrderHeaderId",
+                        column: x => x.OrderHeaderId,
+                        principalTable: "OrderHeaders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Product_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 19, 15, 34, 44, 323, DateTimeKind.Local).AddTicks(7565));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 19, 15, 34, 44, 323, DateTimeKind.Local).AddTicks(7586));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 19, 15, 34, 44, 323, DateTimeKind.Local).AddTicks(7587));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 1,
+                column: "ProductDescription",
+                value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 2,
+                column: "ProductDescription",
+                value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 3,
+                column: "ProductDescription",
+                value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderHeaderId",
+                table: "OrderDetails",
+                column: "OrderHeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "OrderHeaders");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 1,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 11, 14, 35, 5, 785, DateTimeKind.Local).AddTicks(1046));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 2,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 11, 14, 35, 5, 785, DateTimeKind.Local).AddTicks(1062));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_ID",
+                keyValue: 3,
+                column: "CreatedDateTime",
+                value: new DateTime(2023, 12, 11, 14, 35, 5, 785, DateTimeKind.Local).AddTicks(1063));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 1,
+                column: "ProductDescription",
+                value: "1TTTTT");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 2,
+                column: "ProductDescription",
+                value: "2TTTTT");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Product_Id",
+                keyValue: 3,
+                column: "ProductDescription",
+                value: "3TTTTT");
+        }
+    }
+}

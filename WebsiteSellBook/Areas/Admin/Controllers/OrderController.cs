e70d362d1c5db2731309699b44c2ea657bb1d5cd
011223345aA <huyhoang11202@gmail.com>
2023-12-26 00:46:32 +0700

    fillter status order

---
+++ WebsiteSellBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.CommandLine;
+using SellBook.DataAccess.Repository.IRepository;
+using SellBook.Models;
+using SellBook.Utility;
+using System.Diagnostics;
+
+namespace WebsiteSellBook.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = SD.Role_Admin)]
+	public class OrderController : Controller
+	{
+
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API Calls
+
+		[HttpGet]
+		public IActionResult getAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			switch (status)
+			{
+				case "pending":
+					orderHeader = orderHeader.Where(item => item.PaymentStatus == SD.PaymentStatusDelayedPayment); break;
+				case "inprocess":
+					orderHeader = orderHeader.Where(item => item.OrderStatus == SD.StatusInProcess); break;
+				case "completed":
+					orderHeader = orderHeader.Where(item => item.OrderStatus == SD.StatusShipped); break;
+				case "approved":
+					orderHeader = orderHeader.Where(item => item.OrderStatus == SD.StatusApproved); break;
+				default:
+					break;
+			}
+
+			return Json(new { data = orderHeader });
+		}
+
+		#endregion
+	}
+}
---
+++ WebsiteSellBook/wwwroot/js/order.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+$(document).ready(() => {
+    let url = window.location.search;
+    if (url.includes("pending")) {
+        loadDataTable("pending");
+    } else if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    } else if (url.includes("completed")) {
+        loadDataTable("completed");
+    } else if (url.includes("approved")) {
+        loadDataTable("approved");
+    } else {
+        loadDataTable("all");
+    }
+
+})
+
+function loadDataTable(status) {
+    dataTable = $('#tblOrderList').DataTable({
+        "ajax": {
+            url: '/admin/order/getall?status=' + status
+        },
+        "columns": [
+            { data: 'id', "Width": "15%" },
+            { data: 'name', "Width": "15%" },
+            { data: 'phoneNumber', "Width": "10%" },
+            { data: 'applicationUser.email', "Width": "20%" },
+            { data: 'orderStatus', "Width": "20%" },
+            { data: 'orderTotal', "Width": "10%" },
+            {
+                data: 'product_Id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/detail?id=${data}" class="btn btn-success"><i class="bi bi-pencil-square" style="height:30px; cursor:pointer"></i>  Detail</a>
+                    </div>`
+                }, "Width": "15%"
+            }
+
+        ],
+    });
+}
